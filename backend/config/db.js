775761dc32869d3bd0445a965a192849543e8333
Ravi Teja Koviri <websoc.dev@gmail.com>
2024-11-21 12:14:24 +0530

    this is e commerce

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+const connectDB = async () => 
+  {
   try {
+    console.log("tryyyyyy",process.env.MONGO_URI)
     await mongoose.connect(process.env.MONGO_URI);
     console.log(`Successfully connnected to mongoDB 👍`);
   } catch (error) {
